f33890039a7ba75be9faa91041314706eb567c51
Iris Ma <iris_ma999@hotmail.com>
2023-11-01 16:44:47 -0700

    updated nlp

--- NLP/data_spark.py
+++ NLP/data_spark.py
@@ -1,9 +1,35 @@
+import os
 import sys
 assert sys.version_info >= (3, 5) # make sure we have Python 3.5+
+print(sys.executable)
+os.system("pip install transformers")
 
+from transformers import (
+    TokenClassificationPipeline,
+    AutoModelForTokenClassification,
+    AutoTokenizer,
+)
+from transformers.pipelines import AggregationStrategy
+import numpy as np
 from pyspark.sql import SparkSession, functions, types
+from girder.api.describe import Description
 
-# add more functions as necessary
+# Define keyphrase extraction pipeline
+class KeyphraseExtractionPipeline(TokenClassificationPipeline):
+    def __init__(self, model, *args, **kwargs):
+        super().__init__(
+            model=AutoModelForTokenClassification.from_pretrained(model),
+            tokenizer=AutoTokenizer.from_pretrained(model),
+            *args,
+            **kwargs
+        )
+    def postprocess(self, all_outputs):
+        results = super().postprocess(
+            all_outputs=all_outputs,
+            aggregation_strategy=AggregationStrategy.SIMPLE,
+        )
+        return np.unique([result.get("word").strip() for result in results])
+
 
 def main():
     columns = ['Job Title',
@@ -43,11 +69,17 @@ def main():
     sch = types.StructType(sch_cols)
 
     df = spark.read.csv("../Static Data/indeed_full.csv", header=True)
-    df.show()
+
+    keywordsUDF = functions.udf(extractor)
+    df.withColumn("Keywords", keywordsUDF(df['Job Description'])).select("Keywords").show()
 
 if __name__ == '__main__':
     spark = SparkSession.builder.appName('Temperature Range SQL').getOrCreate()
     assert spark.version >= '3.0' # make sure we have Spark 3.0+
     spark.sparkContext.setLogLevel('WARN')
     sc = spark.sparkContext
+
+    model_name = "ml6team/keyphrase-extraction-kbir-inspec"
+    extractor = KeyphraseExtractionPipeline(model=model_name)
+
     main()
