227f428f52e01d6bcda318dda7f42e3ad55509f6
Iris Ma <iris_ma999@hotmail.com>
2023-11-02 13:32:35 -0700

    needs to be debugged for types

--- NLP/data_spark.py
+++ NLP/data_spark.py
@@ -1,8 +1,6 @@
 import os
 import sys
 assert sys.version_info >= (3, 5) # make sure we have Python 3.5+
-print(sys.executable)
-os.system("pip install transformers")
 
 from transformers import (
     TokenClassificationPipeline,
@@ -13,6 +11,21 @@ from transformers.pipelines import AggregationStrategy
 import numpy as np
 from pyspark.sql import SparkSession, functions, types
 from girder.api.describe import Description
+
+from pyspark.sql.types import ArrayType, IntegerType
+from pyspark.sql.functions import udf
+
+from numpy.linalg import norm
+
+def encode(text):
+    enc = distilbert_tokenizer.encode(text)
+    pad = np.pad(enc, (0, max(0, 512 - len(enc))), 'constant')
+    return list(pad)
+
+def cos_sim(job):
+    A = np.array(job)
+    B = np.array(resume_enc)
+    return np.dot(A,B)/(norm(A)*norm(B))
 
 # Define keyphrase extraction pipeline
 class KeyphraseExtractionPipeline(TokenClassificationPipeline):
@@ -30,8 +43,8 @@ class KeyphraseExtractionPipeline(TokenClassificationPipeline):
         )
         return np.unique([result.get("word").strip() for result in results])
 
-
 def main():
+    # read data
     columns = ['Job Title',
                 'Job Description',
                 'Job Type',
@@ -68,12 +81,31 @@ def main():
         sch_cols.append(types.StructField(col, types.StringType()))
     sch = types.StructType(sch_cols)
 
-    df = spark.read.csv("../Static Data/indeed_full.csv", header=True)
+    df = spark.read.csv("../Static Data/indeed.csv", header=True)
+    # filter out null Job Descriptions
+    df_filtered = df = df.where(df['Job Description'].isNotNull())
+    
+    #keywordsUDF = functions.udf(extractor)
+    # tokenize_udf = udf(tokenizer.encode, ArrayType(IntegerType()))
+    tokenize_dbert_udf = udf(encode, ArrayType(IntegerType()))
+    cos_sim_udf = udf(cos_sim, IntegerType())
+    
+    #df_keywords = df_filtered.withColumn("Keywords", keywordsUDF(df['Job Description']))
+    # df_encoding = df_filtered.withColumns({"Encoding_Extractor": tokenize_udf(df['Job Description']),
+    #                         'Encoding_Distilbert': tokenize_dbert_udf(df['Job Description'])})
 
-    keywordsUDF = functions.udf(extractor)
-    df.withColumn("Keywords", keywordsUDF(df['Job Description'])).select("Keywords").show()
+    # df_encoding.select(['Job Description', 'Encoding_Extractor', 'Encoding_Distilbert']).show(truncate=False)
+
+    df_encoding = df_filtered.withColumn('Encoding_Distilbert', tokenize_dbert_udf(df['Job Description']))
+    df_cos_sim = df_encoding.withColumn('Cosine_Similarity', cos_sim_udf(df_encoding['Encoding_Distilbert']))\
+        .orderBy('Cosine_Similarity', ascending=False)
+
+    results = df_cos_sim.take(10)
+    print(results)
 
 if __name__ == '__main__':
+
+    # set up
     spark = SparkSession.builder.appName('Temperature Range SQL').getOrCreate()
     assert spark.version >= '3.0' # make sure we have Spark 3.0+
     spark.sparkContext.setLogLevel('WARN')
@@ -81,5 +113,12 @@ if __name__ == '__main__':
 
     model_name = "ml6team/keyphrase-extraction-kbir-inspec"
     extractor = KeyphraseExtractionPipeline(model=model_name)
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
+
+    distilbert_tokenizer = AutoTokenizer.from_pretrained("distilbert-base-uncased")
+
+    # sample resume
+    resume = 'Software'
+    resume_enc = encode(resume)
 
     main()
