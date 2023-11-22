7d3f1f0e5080eb5fdbdcb065d04535aed38272ae
chen wang <chen.wang.ms@outlook.com>
2023-11-21 18:43:12 -0800

    provide demo for get and post command

--- Django/Django/settings.py
+++ Django/Django/settings.py
@@ -26,7 +26,10 @@ SECRET_KEY = 'django-insecure-n-201l(!ndq1s_rfs$v%9)(ytq@d&$u0@@kl&p_1neg4bp#89p
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
+CORS_ALLOW_ALL_ORIGINS = False
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:8080',
+]
 
 # Application definition
 
@@ -37,12 +40,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'upload'
+    'upload',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
